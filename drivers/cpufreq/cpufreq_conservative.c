722d3f3581b84369e5aa45d7c334c45081bf5861
mydongistiny <jaysonedson@gmail.com>
2016-07-12 02:06:02 -0700

    Revert "conservative: add support for saving tunables per policy governor instance"
    This reverts commit dbec1f80adc820cd0e8f519f2432c823fd5a749f.

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -23,8 +23,6 @@
 
 static DEFINE_PER_CPU(struct cs_cpu_dbs_info_s, cs_cpu_dbs_info);
 
-static DEFINE_PER_CPU(struct cs_dbs_tuners *, cached_tuners);
-
 static inline unsigned int get_freq_target(struct cs_dbs_tuners *cs_tuners,
 					   struct cpufreq_policy *policy)
 {
@@ -319,29 +317,14 @@ static struct attribute_group cs_attr_group_gov_pol = {
 
 /************************** sysfs end ************************/
 
-static void save_tuners(struct cpufreq_policy *policy,
-		struct cs_dbs_tuners *tuners)
-{
-	int cpu;
-
-	if (have_governor_per_policy())
-	    cpu = cpumask_first(policy->related_cpus);
-	else
-	    cpu = 0;
-
-	WARN_ON(per_cpu(cached_tuners, cpu) &&
-	per_cpu(cached_tuners, cpu) != tuners);
-	per_cpu(cached_tuners, cpu) = tuners;
-}
-
-static struct cs_dbs_tuners *alloc_tuners(struct cpufreq_policy *policy)
+static int cs_init(struct dbs_data *dbs_data)
 {
 	struct cs_dbs_tuners *tuners;
 
 	tuners = kzalloc(sizeof(*tuners), GFP_KERNEL);
 	if (!tuners) {
-	    pr_err("%s: kzalloc failed\n", __func__);
-	    return ERR_PTR(-ENOMEM);
+		pr_err("%s: kzalloc failed\n", __func__);
+		return -ENOMEM;
 	}
 
 	tuners->up_threshold = DEF_FREQUENCY_UP_THRESHOLD;
@@ -349,34 +332,6 @@ static struct cs_dbs_tuners *alloc_tuners(struct cpufreq_policy *policy)
 	tuners->sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR;
 	tuners->ignore_nice_load = 0;
 	tuners->freq_step = DEF_FREQUENCY_STEP;
-
-	save_tuners(policy, tuners);
-
-	return tuners;
-}
-
-static struct cs_dbs_tuners *restore_tuners(struct cpufreq_policy *policy)
-{
-	int cpu;
-
-	if (have_governor_per_policy())
-	    cpu = cpumask_first(policy->related_cpus);
-	else
-	    cpu = 0;
-
-	return per_cpu(cached_tuners, cpu);
-}
-
-static int cs_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
-{
-	struct cs_dbs_tuners *tuners;
-
-	tuners = restore_tuners(policy);
-	if (!tuners) {
-		tuners = alloc_tuners(policy);
-		if (IS_ERR(tuners))
-			return PTR_ERR(tuners);
-	}
 
 	dbs_data->tuners = tuners;
 	dbs_data->min_sampling_rate = MIN_SAMPLING_RATE_RATIO *
@@ -387,7 +342,7 @@ static int cs_init(struct dbs_data *dbs_data, struct cpufreq_policy *policy)
 
 static void cs_exit(struct dbs_data *dbs_data)
 {
-//	nothing to do
+	kfree(dbs_data->tuners);
 }
 
 define_get_cpu_dbs_routines(cs_cpu_dbs_info);
@@ -409,7 +364,7 @@ static struct common_dbs_data cs_dbs_cdata = {
 	.gov_dbs_timer = cs_dbs_timer,
 	.gov_check_cpu = cs_check_cpu,
 	.gov_ops = &cs_ops,
-	.init_cs = cs_init,
+	.init = cs_init,
 	.exit = cs_exit,
 };
 
@@ -436,13 +391,7 @@ static int __init cpufreq_gov_dbs_init(void)
 
 static void __exit cpufreq_gov_dbs_exit(void)
 {
-	int cpu;
-
 	cpufreq_unregister_governor(&cpufreq_gov_conservative);
-	for_each_possible_cpu(cpu) {
-		kfree(per_cpu(cached_tuners, cpu));
-		per_cpu(cached_tuners, cpu) = NULL;
-	}
 }
 
 MODULE_AUTHOR("Alexander Clouter <alex@digriz.org.uk>");
